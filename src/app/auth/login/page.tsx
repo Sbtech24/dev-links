289580afc26e30b9ce3f19ff1c7a74a58f6c9379
Sbtech24 <bajomosemilore@gmail.com>
2025-09-30 00:59:58 +0100

    Add layout components for login and registration pages
    - Created LoginLayout component with metadata for the login page.
    - Created RegisterLayout component with metadata for the registration page.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,37 +2,47 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import {Envelope,LockKey} from "@phosphor-icons/react";
+import {EnvelopeSimpleIcon,LockKeyIcon} from "@phosphor-icons/react";
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+
+const LoginPage = () => {
+    const router = useRouter()
+
+    // Redirect to home 
+    function RedirectToHome(){
+        router.push("/")
+
+    }
+
   return (
-    <div className='sm:max-w-md mx-auto p-3 md:flex md:flex-col md:gap-4 md:items-center md:justify-center h-screen'>
-        <header className='flex items-center'>
+    <div className='p-7 sm:max-w-md mx-auto md:flex md:flex-col  md:gap-4 md:items-center md:justify-center h-screen'>
+        <header className='pb-4 flex items-center'>
             <Image src={`/devlinks-logo.svg`} alt="" width={40} height={40}/>
-            <h1 className='font-bold text-2xl text-darkGrey'>devlinks</h1>
+            <h1 className='font-bold text-3xl text-darkGrey'>devlinks</h1>
         </header>
 
         <div className='pt-4 sm:bg-white sm:p-9 rounded-md'>
             <h3 className='font-bold pb-2 text-darkGrey text-xl sm:text-2xl'>Login</h3>
-            <p className='text-grey pb-2 text-sm leading-6 mb-3'>Add your details below to get back into the app</p>
+            <p className='text-grey pb-2 text-base leading-6 mb-3'>Add your details below to get back into the app</p>
 
-            <form action="" className='flex flex-col gap-3'>
+            <form action="" className='flex flex-col gap-5'>
                 <label htmlFor="" className='block text-sm text-darkGrey'>Email address
                 <div className="relative w-full ">
-                    <Envelope className="absolute left-3 top-1/3 -translate-y-1" size={20} color='#737373'/>
+                    <EnvelopeSimpleIcon weight='fill' className="absolute left-3 top-1/3 -translate-y-1" size={20} color='#737373'/>
                     <input type="email" name="Email address" id="" className='input-style text-[#333333]' placeholder='e.g.alex@email.com'/>  
                 </div>
                 </label>
 
                 <label htmlFor="password" className='block text-sm text-darkGrey'>Password
                 <div className='relative w-full'>
-                    <LockKey className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
+                    <LockKeyIcon weight='fill' className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
                 <input type="password" name="Password" id="" className='input-style text-[#333333]' placeholder='Enter your password' />
                     
                     </div> 
                </label>
                 
-                <button className='text-white bg-purple w-full p-2 mt-2 mb-1 rounded-md'>Login</button>
+                <button className='text-white bg-purple w-full p-2 mt-2 mb-1 rounded-md' onClick={RedirectToHome}>Login</button>
             </form>
 
             <div className='flex flex-col justify-center text-center mt-3 sm:flex-row gap-2'>
@@ -44,4 +54,4 @@ const page = () => {
   )
 }
 
-export default page
+export default LoginPage
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,50 +1,50 @@
 "use client"
+import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import {Envelope,LockKey} from "@phosphor-icons/react";
+import {EnvelopeSimpleIcon,LockKeyIcon} from "@phosphor-icons/react";
 
-
-const Register = () => {
+const RegisterPage = () => {
   return (
-     <div className='sm:max-w-md mx-auto p-3 md:flex md:flex-col md:gap-4 md:items-center md:justify-center h-screen'>
-        <header className='flex items-center'>
+    <div className='p-7 sm:max-w-md mx-auto md:flex md:flex-col  md:gap-4 md:items-center md:justify-center h-screen'>
+        <header className='pb-4 flex items-center'>
             <Image src={`/devlinks-logo.svg`} alt="" width={40} height={40}/>
-            <h1 className='font-bold text-2xl text-darkGrey'>devlinks</h1>
+            <h1 className='font-bold text-3xl text-darkGrey'>devlinks</h1>
         </header>
 
         <div className='pt-4 sm:bg-white sm:p-9 rounded-md'>
             <h3 className='font-bold pb-2 text-darkGrey text-xl sm:text-2xl'>Create Account</h3>
-            <p className='text-grey pb-2 text-sm leading-6 mb-3'>Let’s get you started sharing your links!</p>
+            <p className='text-grey pb-2 text-base leading-6 mb-3'>Let's Get you started sharing your links</p>
 
-            <form action="" className='flex flex-col gap-3'>
+            <form action="" className='flex flex-col gap-5'>
                 <label htmlFor="" className='block text-sm text-darkGrey'>Email address
                 <div className="relative w-full ">
-                    <Envelope className="absolute left-3 top-1/3 -translate-y-1" size={20} color='#737373'/>
+                    <EnvelopeSimpleIcon weight='fill' className="absolute left-3 top-1/3 -translate-y-1" size={20} color='#737373'/>
                     <input type="email" name="Email address" id="" className='input-style text-[#333333]' placeholder='e.g.alex@email.com'/>  
                 </div>
                 </label>
 
                 <label htmlFor="password" className='block text-sm text-darkGrey'>Password
                 <div className='relative w-full'>
-                    <LockKey className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
-                <input type="password" name="Password" id="" className='input-style text-[#333333]' placeholder='At least 8 characters' />
+                    <LockKeyIcon weight='fill' className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
+                <input type="password" name="Password" id="" className='input-style text-[#333333]' placeholder='At least .8 Characters' />
                     
                     </div> 
                </label>
 
-                <label htmlFor="confirm-password" className='block text-sm text-darkGrey'>Confirm Password
+                <label htmlFor="confirm-password" className='block text-sm text-darkGrey '>Confirm Password
                 <div className='relative w-full'>
-                    <LockKey className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
-                <input type="password" name="Password" id="" className='input-style text-[#333333]' placeholder='At least 8 characters' />
+                    <LockKeyIcon weight='fill' className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
+                <input type="password" name="confirm-password" id="" className='input-style text-[#333333]' placeholder='At least .8 Characters' />
                     
                     </div> 
                </label>
                 
-                <button className='text-white bg-purple w-full p-2 mt-2 mb-1 rounded-md'>Create New Account</button>
+                <button className='text-white bg-purple w-full p-2 mt-2 mb-1 rounded-md '>Create new account</button>
             </form>
 
             <div className='flex flex-col justify-center text-center mt-3 sm:flex-row gap-2'>
-                <p className='text-grey'>Already have an account?</p>
+                <p className='text-grey'>Already Have an account?</p>
                 <Link href="/auth/login" className='text-purple '>Login</Link>
             </div>
         </div>
@@ -52,4 +52,4 @@ const Register = () => {
   )
 }
 
-export default Register
+export default RegisterPage
