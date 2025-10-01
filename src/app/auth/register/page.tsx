d7ab796a08a66f60845b92c6ff28ad982ebf3aca
Sbtech24 <bajomosemilore@gmail.com>
2025-10-02 00:50:21 +0100

    feat: integrate Supabase for authentication; add middleware and client setup

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,8 +3,32 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import {EnvelopeSimpleIcon,LockKeyIcon} from "@phosphor-icons/react";
+import { createClient } from '@/utils/superbase/client';
+import { useForm,SubmitHandler } from 'react-hook-form';
+import { useState } from 'react';
+
+interface FormType{
+    email:string,
+    password:string,
+    confirmPassword?:string
+}
+
+const superbase = createClient()
 
 const RegisterPage = () => {
+    const {register,handleSubmit} = useForm<FormType>()
+     const [message, setMessage] = useState("");
+    const onSubmit: SubmitHandler<FormType> = async (data) =>{
+        const {error} = await superbase.auth.signUp({
+            email:data.email,
+            password:data.password
+
+          
+        })
+        setMessage(error ? error.message : "Check your email to confirm your account.");
+
+    }
+
   return (
     <div className='p-7 sm:max-w-md mx-auto md:flex md:flex-col  md:gap-4 md:items-center md:justify-center h-screen'>
         <header className='pb-4 flex items-center'>
@@ -16,32 +40,33 @@ const RegisterPage = () => {
             <h3 className='font-bold pb-2 text-darkGrey text-xl sm:text-2xl'>Create Account</h3>
             <p className='text-grey pb-2 text-base leading-6 mb-3'>Let's Get you started sharing your links</p>
 
-            <form action="" className='flex flex-col gap-5'>
-                <label htmlFor="" className='block text-sm text-darkGrey'>Email address
+            <form action="" className='flex flex-col gap-5' onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email" className='block text-sm text-darkGrey'>Email address
                 <div className="relative w-full ">
-                    <EnvelopeSimpleIcon weight='fill' className="absolute left-3 top-1/3 -translate-y-1" size={20} color='#737373'/>
-                    <input type="email" name="Email address" id="" className='input-style text-[#333333]' placeholder='e.g.alex@email.com'/>  
+                    {/* <EnvelopeSimpleIcon weight='fill' className="absolute left-3 top-1/2 -translate-y-1/2" size={20} color='#737373'/> */}
+                    <input type="email" {...register("email",{required:true})} id="email" className='input-style pl-12 text-[#333333]' placeholder='e.g.alex@email.com'/>  
                 </div>
                 </label>
 
                 <label htmlFor="password" className='block text-sm text-darkGrey'>Password
                 <div className='relative w-full'>
-                    <LockKeyIcon weight='fill' className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
-                <input type="password" name="Password" id="" className='input-style text-[#333333]' placeholder='At least .8 Characters' />
+                    {/* <LockKeyIcon weight='fill' className='absolute top-1/2 left-3 -translate-y-1/2' size={20} color='#737373'/> */}
+                <input type="password" {...register("password",{required:true,max:8})} id="password" className='h-10 pl-12 input-style text-[#333333]' placeholder='At least .8 Characters' />
                     
                     </div> 
                </label>
 
                 <label htmlFor="confirm-password" className='block text-sm text-darkGrey '>Confirm Password
                 <div className='relative w-full'>
-                    <LockKeyIcon weight='fill' className='absolute top-1/3 left-3 -translate-y-1' size={20} color='#737373'/>
-                <input type="password" name="confirm-password" id="" className='input-style text-[#333333]' placeholder='At least .8 Characters' />
+                    {/* <LockKeyIcon weight='fill' className='absolute top-1/2 left-3 -translate-y-1/2' size={20} color='#737373'/> */}
+                <input type="password" {...register("confirmPassword",{required:true,max:8})} id="confirm-password" className='h-10 pl-12 input-style text-[#333333]' placeholder='At least .8 Characters' />
                     
                     </div> 
                </label>
                 
-                <button className='text-white bg-purple w-full p-2 mt-2 mb-1 rounded-md '>Create new account</button>
+                <button className='text-white bg-purple w-full p-2 mt-2 mb-1 rounded-md'>Create new account</button>
             </form>
+              {message && <p>{message}</p>}
 
             <div className='flex flex-col justify-center text-center mt-3 sm:flex-row gap-2'>
                 <p className='text-grey'>Already Have an account?</p>
